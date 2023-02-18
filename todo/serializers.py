a98d509d159fd6b0508ab7da066b314c18a4fe32
Daniel Rubin <daniel.rubin1997@gmail.com>
2023-02-18 18:33:49 +0100

    fixed Hyperlinked error

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Todo
 
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class TodoSerializer(serializers.Serializer):
     class Meta:
         model = Todo
         fields = ['title', 'description', 'due_date', 'status', 'user']
